c22cbbd59b769924aed48001d25cc8b87b75ebde
praveensayani <praveen8apr@gmail.com>
2017-11-20 11:27:28 -0800

    Files added

---
+++ ANT_Original_code/Originalcode.java
@@ -0,0 +1,7 @@
+class Originalcode
+{
+public static void main(String[]args)
+{
+System.out.println("Welcome to Originalcode");
+}
+}
---
+++ ANT_Properties/Properties.java
@@ -0,0 +1,7 @@
+class Properties
+{
+public static void main(String[]args)
+{
+System.out.println("Welcome to Properties");
+}
+}
